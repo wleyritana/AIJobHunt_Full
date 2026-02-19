08728d78df99c4837cb9ffc448d6aeb1216fbd51
wleyritana <leyritana.wilfredo.se@gmail.com>
2026-02-20 00:44:15 +0800

    Delete app directory

--- app/services/scoring.py
+++
@@ -1,7 +0,0 @@
-def weighted_score(skill_match: float, experience: float, tools: float, impact: float) -> float:
-    return (
-        skill_match * 0.35 +
-        experience * 0.20 +
-        tools * 0.15 +
-        impact * 0.30
-    )
